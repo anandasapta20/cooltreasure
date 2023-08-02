306bc134edb8056337d6e9bca586c1d141c2679b
anandasapta20 <anandaaweu20@gmail.com>
2023-08-02 22:50:52 +0700

    change register success message

--- app/Views/success/registerSuccess.php
+++ app/Views/success/registerSuccess.php
@@ -6,7 +6,7 @@
         <div class="col-6 text-center">
             <img src="/Assets/success-daftar.png" alt="daftar berhasil" height="360px">
             <h1><b>Akun Telah Berhasil Dibuat!</b></h1>
-            <p>Masukkan alamat email dan kata sandi anda pada halaman masuk untuk menggunakan fitur yang lebih lengkap.</p>
+            <p>Aktifkan akunmu dengan lakukan klik link pada pesan email yang telah dikirimkan ke akun emailmu.</p>
             <div class="d-grid mt-5">
                 <a href="/masuk" class="btn btn-1 px-auto">Lanjut ke Halaman Masuk</a>
             </div>
